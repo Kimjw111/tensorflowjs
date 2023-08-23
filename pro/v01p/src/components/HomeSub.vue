caa46ae4be8ef4eea878c2095ed803a981b87a45
Kimjw111 <whale960105@gmail.com>
2023-08-23 13:01:08 +0900

    vue 데이터 전송

---
+++ pro/v01p/src/components/HomeSub.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h3>여기는 homeSub데이터 입니다.</h3>
+    <div>
+      <mark>받은데이터: {{ homesub }}</mark>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: ['homesub'],
+  data() {
+    return {
+      //   test: '아직테스트중'
+    }
+  }
+}
+</script>
+<style></style>
